b1e6dc9dfdb0f0db5b5a1033e6c94bd6f14018b6
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-12-03 11:03:50 -0700

    Update Exercise9_1
    Added header and finalized code

--- Chapter9/Exercise9_1.java
+++ Chapter9/Exercise9_1.java
@@ -1,20 +1,28 @@
+// Author Jason Waters
+// Date 12/3/2019
+// CSCI_1110_Assignment_9_1
+
+
+
 public class Rectangle{
 	
 	double width;
-	double heigth;
-	
+	double height;
+	double area;
+	double peri;
+
 	public Rectangle(){
 		this.width = 1;
-		this.heigth = 1;
+		this.height = 1;
 	}
-	public Rectangle(double width, double heigth){
+	public Rectangle(double width, double height){
 		this.width = width;
 		this.height = height;
 	}
 	public void	setWidth(double newWidth){
 		this.width = newWidth;
 	}
-	public void setHeigth(double newHeigth){
+	public void setHeigth(double newHeight){
 		this.height = newHeight;
 	}
 	public double getWidth(){
@@ -30,14 +38,3 @@ public class Rectangle{
 		return (this.width * 2) + (this.height * 2);
 	}
 }
-
-
-/*(The Rectangle class) Following the example of the Circle class in Section 9.2, design a class named Rectangle to represent a rectangle. The class contains:
-
-Two double data fields named width and height that specify the width and height of the rectangle. The default values are 1 for both width and height.
-A no-arg constructor that creates a default rectangle.
-A constructor that creates a rectangle with the specified width and height.
-A method named getArea() that returns the area of this rectangle.
-A method named getPerimeter() that returns the perimeter.
-Create the Rectangle object class. Write a test program that creates two Rectangle objects—one with width 4 and height 40 and the other with width 3.5 and height 35.9. Display the width, height, area, and perimeter of each rectangle in this order.
-*/
