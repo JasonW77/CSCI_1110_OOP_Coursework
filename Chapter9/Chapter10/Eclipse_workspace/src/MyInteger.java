236c740fad2b018680a90bbf146e3a32b9630abf
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-12-05 11:56:01 -0700

    Added files for chapter 10

---
+++ Chapter9/Chapter10/Eclipse_workspace/src/MyInteger.java
@@ -0,0 +1,70 @@
+// Author Jason Waters
+// Date 12/5/2019
+// CSCI_1110_Assignment_10_3
+
+
+public class MyInteger {
+	
+	private int value = 1;
+	
+	public MyInteger() {
+		this.value = value;
+	}
+	
+	public int getMyInteger() {
+		return this.value;
+	}
+	
+	public boolean isEven() {
+		
+	}
+	
+	public boolean isOdd() {
+		
+	}
+	
+	public boolean isPrime() {
+		
+	}
+	
+	public static boolean isEven(int value) {
+		
+	}
+	
+	public static boolean isOdd(int value) {
+		
+	}
+	
+	public static boolean isPrime(int value) {
+		
+	}
+	
+	public static boolean isEven(MyInteger value) {
+		
+	}
+	
+	public static boolean isMyOdd(MyInteger value) {
+		
+	}
+	
+	public static boolean isMyPrime(MyInteger value) {
+		
+	}
+	
+	public boolean equals() {
+		
+	}
+	
+	public boolean equals(MyInteger value) {
+		
+	}
+	
+	public static parseInt(char[]) {
+		
+	}
+	
+	public static parseInt(String) {
+		
+	}
+
+
