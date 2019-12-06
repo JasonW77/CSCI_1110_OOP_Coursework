a74d801781ad3fc201a5196d69f15b75d40d5dd7
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-12-06 10:45:15 -0700

    Update MyIntegerObject.java

--- Chapter10/Exercise10_3/MyIntegerObject.java
+++ Chapter10/Exercise10_3/MyIntegerObject.java
@@ -1,4 +1,50 @@
 
 public class MyIntegerObject {
-
+	
+	public static void main(String args[]) {
+		
+		//Constructor
+		MyInteger def;
+		def = new MyInteger();
+		
+		//getter method
+		def.getMyInteger();
+		
+		//isEven() method
+		def.isEven();
+		
+		//isPrime() method
+		def.isPrime();
+		
+		//isOdd() method
+		def.isOdd();
+		
+		//isEven(int) method static
+		MyInteger.isIntEven(def);
+		
+		//isPrime(int) method static
+		MyInteger.isIntPrime(def);
+		
+		//isOdd(int) method static
+		MyInteger.isIntOdd(def);
+		
+		//isEven(MyInteger) method static
+		MyInteger.isMyEven(def);
+		
+		//isPrime(MyInteger) method static
+		MyInteger.isMyPrime(def);
+		
+		//isOdd(MyInteger) method static
+		MyInteger.isMyOdd(def);
+		
+		//equals(int) method
+		
+		//equals(MyInteger) method
+		
+		//parseInt(char[]) method
+		
+		//parseInt(String) method
+		
+		
+	}
 }
