01d3d2b75b079e2b0ebcfdf2e2b9c38079533b9e
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2019-12-19 09:37:50 -0700

    Update triangle.java

--- Chapter11/src/triangle.java
+++ Chapter11/src/triangle.java
@@ -1,14 +1,14 @@
+//Author Jason Waters
+//Date 12/19/2019
+//CSCI_1110_Exercise_11_1 
+
 
 public class triangle extends GeometricObject{
 	private double side1 = 1.0;
 	private double side2 = 1.0;
 	private double side3 = 1.0;
 	
-	public triangle() {
-		
-	}
-	
-	public void triangle(double side1, double side2, double side3) {
+	public triangle(double side1, double side2, double side3) {
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
@@ -31,12 +31,10 @@ public class triangle extends GeometricObject{
 	}
 	public double getPerimeter() {
 		double perimeter = 0;
-		perimeter = (side1+side2+side3);
+		perimeter = (side1 + side2 + side3);
 		return perimeter;
 	}
 	public String toString() {
-		return "Triangle: side1 = " + side1 + " side2 = " + side2 + "side3 = " + side3;
+		return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
 	}
-
-	
 }
