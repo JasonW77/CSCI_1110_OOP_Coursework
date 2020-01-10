a7708e4bf32b6b2edf44d6df35525448551a52d0
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-01-09 22:12:34 -0700

    Create GeometricObject.java

---
+++ Chapter13/Exercise13_11/GeometricObject.java
@@ -0,0 +1,44 @@
+//Author Jason Waters
+//Date 1/8/2020
+//CSCI_1110_Exercise_13_7
+
+
+
+public abstract class GeometricObject{
+	private String color = "white";
+	private boolean filled;
+	private java.util.Date dateCreated;
+	
+	public GeometricObject() {
+		dateCreated = new java.util.Date();
+	}
+	public GeometricObject(String color, boolean filled) {
+		dateCreated = new java.util.Date();
+		this.color = color;
+		this.filled = filled;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public boolean isFilled() {
+		return filled;
+	}
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+	public java.util.Date getDateCreated(){
+		return dateCreated;
+	}
+	public String toString() {
+		return "created on " + dateCreated + "\ncolor: " + color + " and filled: " + filled;
+	}
+	public abstract double getArea() {
+		return area;
+	}
+	public abstract double getPerimeter() {
+		return perimeter;
+	}
+}
