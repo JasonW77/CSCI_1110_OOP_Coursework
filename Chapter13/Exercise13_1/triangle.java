6843b729a0186db78b60ce2a3803d32a4f1f3b9a
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-01-13 10:16:08 -0700

    Update triangle.java

--- Chapter13/Exercise13_1/triangle.java
+++ Chapter13/Exercise13_1/triangle.java
@@ -5,17 +5,17 @@ package exercise13_1;
 //CSCI_1110_Exercise_13_1
 
 
-public class triangle extends GeometricObject{
+public class Triangle extends GeometricObject{
 	private double side1 = 1.0;
 	private double side2 = 1.0;
 	private double side3 = 1.0;
 	
-	public triangle(){
+	public Triangle(){
 		this.side1 = 1.0;
 		this.side2 = 1.0;
 		this.side3 = 1.0;
 	}
-	public triangle(double side1, double side2, double side3) {
+	public Triangle(double side1, double side2, double side3) {
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
