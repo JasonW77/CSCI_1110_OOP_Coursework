de4202490dff9483383ee25b2a79576b3da5e1ce
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-01-13 09:13:41 -0700

    Update TriangleTest.java

--- Chapter13/Exercise13_1/TriangleTest.java
+++ Chapter13/Exercise13_1/TriangleTest.java
@@ -1,6 +1,8 @@
+package exercise13_1;
+
 //Author Jason Waters
-//Date 12/19/2019
-//CSCI_1110_Exercise_11_1 
+//Date 1/13/2020
+//CSCI_1110_Exercise_13_1 
 
 import java.util.Scanner;
 public class TriangleTest{
@@ -36,7 +38,7 @@ public class TriangleTest{
 		System.out.println("Area of the triangle is: " + tri.getArea());
 		System.out.println("Perimeter of the triangle is: " + tri.getPerimeter());
 		System.out.println("Color of Triangle is: " +tri.getColor());
-		System.out.println("Date Created: " + tri.getDateCreated());
+		//System.out.println("Date Created: " + tri.getDateCreated());
 		System.out.println("Trinangle filled: " + tri.isFilled());
 		System.out.println(tri.toString());
 		
