97a936b49ed0d069337ae043bf165e4e7209ab8c
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-01-13 10:05:47 -0700

    Update and rename colorable.java to Colorable.java

---
+++ Chapter13/Exercise13_7/Colorable.java
@@ -0,0 +1,10 @@
+package exercise13_7;
+
+//Author Jason Waters
+//Date 1/13/2020
+//CSCI_1110_Exercise_13_7
+
+
+public interface Colorable{
+public abstract String howToColor();
+}
--- Chapter13/Exercise13_7/colorable.java
+++
@@ -1,8 +0,0 @@
-//Author Jason Waters
-//Date 1/13/2020
-//CSCI_1110_Exercise_13_7
-
-
-public interface colorable{
-  public abstract String howToColor();
-  }
