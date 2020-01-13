60c1ea29d3685711cce54604464d13ec57922542
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-01-13 09:22:18 -0700

    Update triangle.java

--- Chapter13/Exercise13_7/triangle.java
+++ Chapter13/Exercise13_7/triangle.java
@@ -1,6 +1,6 @@
 //Author Jason Waters
-//Date 1/8/2020
-//CSCI_1110_Exercise_13_1
+//Date 1/13/2020
+//CSCI_1110_Exercise_13_7
 
 
 public class triangle extends GeometricObject implements colorable{
