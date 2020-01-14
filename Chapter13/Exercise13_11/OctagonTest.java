5eb12d683fa79eb7b8ca055d4251c5f4912c3270
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-01-14 10:29:53 -0700

    Update OctagonTest.java

--- Chapter13/Exercise13_11/OctagonTest.java
+++ Chapter13/Exercise13_11/OctagonTest.java
@@ -1,21 +1,22 @@
 //Author Jason Waters
-//Date 1/8/2020
+//Date 1/14/2020
 //CSCI_1110_Exercise_13_11
 
 package exercise13_11;
 
 public class OctagonTest {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws CloneNotSupportedException {
 		
 		//Octagon oct = new Octagon();
 		
-		Octagon oct = new Octagon(2,2,2,2,2,2,2,2);
+		Octagon oct1 = new Octagon(2,2,2,2,2,2,2,2);
 		
-		//oct.clone();
+		Octagon oct2 = (Octagon)oct1.clone() ;
 		
-		System.out.println("Area of octogon is: " + oct.getArea());
-		System.out.println(oct.compare());
-		System.out.println(oct.toString());
-	
+		System.out.println("Area of oct1 is: " + oct1.getArea());
+		System.out.println("Area of oct2 is: " + oct2.getArea());
+		System.out.println(oct1.compareTo(oct2));
+		System.out.println(oct1.toString());
+		System.out.println(oct2.toString());
 	}
 }
