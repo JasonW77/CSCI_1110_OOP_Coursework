7c1e7c9bb8d73813cf93f016ffcca1fc0a83ddf7
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-02-05 11:49:23 -0700

    cleaned up code and clarified the notes

--- Chapter17/Exercise17_14.java
+++ Chapter17/Exercise17_14.java
@@ -15,39 +15,35 @@ public class Exercise17_14 {
 	public static void main(String args[]) throws IOException {
 		int value;
 		ArrayList<Integer> list = new ArrayList<>();
-		//get input from user.
 		String from = " ";
 		String to = " ";
+		
+		//get file name input from user.
 		System.out.print("Enter a File name to encrypt from: ");
 		Scanner scan = new Scanner(System.in);
 		from = scan.next();
 		System.out.print("Enter a File name to decrypt to: ");
 		to = scan.next();
 		
-		// write non-encrypted from file
+		// create and/or write to non-encrypted "from" file
 		try (FileOutputStream output = new FileOutputStream(from)){
 			for (int i = 1; i <= 10; i++)
 				output.write(i);
 		}
 		
-		//read file to the array list and encode by adding 5
+		// read "from" file to the array list and encode by adding 5
 		try	(FileInputStream input = new FileInputStream(from)){
 			while ((value = input.read()) != -1)
 				list.add(value + 5);
-			/*	
-			 //this is here just to show what is printed to the array list.
-			for (int i2 = 0; i2 < list.size(); i2++)
-				System.out.print(list.get(i2) + " ");
-			*/
 		}
-		// read the array list and encrypt to new file
+		
+		// read data from the array list and encrypt back to the "from" file
 		try (FileOutputStream output = new FileOutputStream(from)){
 			for (int i = 0; i < list.size(); i++)
 				output.write(list.get(i));
-			
 		}
 		
-		// read and undo-encryption from file, write to file
+		// read "from" file and undo-encryption by subtracting 5, write data to "to" file
 		try {
 			FileOutputStream output = new FileOutputStream(to);
 			FileInputStream input = new FileInputStream(from);
