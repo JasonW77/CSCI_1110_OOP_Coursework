0e3b47521cb65c67d508162c670187fb8e3e98a7
JasonW77 <56334834+JasonW77@users.noreply.github.com>
2020-02-24 12:50:27 -0700

    Add files via upload

---
+++ Final Project/Final Project/src/battleship/GameCounter.java
@@ -0,0 +1,81 @@
+//Author Jason Waters
+//Date 2/20/2020
+//CSCI_1110_Final_Project
+
+package battleship;
+
+public class GameCounter extends Interface{
+	
+	private int playerID;
+	private String Name;
+	private int loss;
+	private int win;
+	private int tie;
+	private java.util.Date date = new java.util.Date();
+	
+	//default Object creation settings
+	public GameCounter(){
+		this.playerID = 0;
+		this.Name = "Player";
+		this.loss = 0;
+		this.win = 0;
+		this.tie = 0;
+		this.date = new java.util.Date();
+	}
+	
+	//Object creation with specified settings
+	public GameCounter(int id, String Name, int win, int loss, int tie){
+		this.playerID = id;
+		this.Name = Name;
+		this.win = win;
+		this.loss = loss;
+		this.tie = tie;
+		this.date = new java.util.Date();
+	}
+	
+	//Player ID getter and setter
+	public int getPlayerID(){
+		return this.playerID;
+	}
+	public void setPlayerID(){
+		this.playerID++;
+	}
+	
+	//method for Game Start date
+	public java.util.Date getDate(){
+		return this.date;
+	}
+	
+	//Player Name getter and setter
+	public String getName(){
+		return this.Name;
+	}
+	public void setName(String Name){
+		this.Name = Name;
+	}
+	
+	//Player Win getter and setter
+	public int getWin(){
+		return this.win;
+	}
+	public void setWin(){
+		this.win++;
+	}
+	
+	//Player loss getter and setter
+	public int getLoss(){
+		return this.loss;
+	}
+	public void setLoss(){
+		this.loss++;
+	}
+	 
+	//Player Tie getter and setter, even though A tie is not possible in this game...
+	public int getTie(){
+		return this.tie;
+	}
+	public void setTie(){
+		this.tie++;
+	}
+	
+}
